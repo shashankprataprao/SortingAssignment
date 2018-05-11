eea53ca2360139462bfb16dfe8e70f49a2d0478d
Shashank Rao <Shashank Rao@DESKTOP-S5KOQ7H>
2018-05-11 13:03:43 +0530

    updated

--- src/client/Client.java
+++ src/client/Client.java
@@ -9,11 +9,31 @@ public class Client {
 
 	public static void main(String[] args) {
 		
-		int[] arr = {1,2,3,4,8,5,4,10,17,33,21,19,16};
+		
+		
+		//int[] arr = {1,2,3,4,8,5,4,10,17,33,21,19,16};
 		int ch;
 		
 		try(Scanner sc  = new Scanner(System.in);) {
 			do {
+				System.out.println(" ");
+				System.out.println("ENTER THE LENGTH OF ARRAY");
+				System.out.print("length of array =");
+				int n = sc.nextInt();
+				int[] arr = new int[n];
+				
+				System.out.println("Now enter the elements of array with size :"+n);
+				for (int i = 0 ; i < arr.length ; i++) {
+					arr[i]=sc.nextInt();
+					
+				}
+				
+				System.out.println("your entered array is ");
+				for (int i : arr) {
+					System.out.print("   "+i);
+				}
+				
+				
 				
 				System.out.println(" ");
 				System.out.println("ENTER CHOICE");
